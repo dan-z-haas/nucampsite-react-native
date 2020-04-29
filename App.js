8035f138a7c27dc6aa7954b6db6e22a77752da1a
Dan Haas <dan.z.haas@gmail.com>
2020-04-28 22:19:33 -0400

    Navigation icons and Custom side drawer

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Main from './components/MainComponent';
 import { YellowBox } from 'react-native';
 
-//YellowBox.ignoreWarnings(['Warning: ...'])
-//console.disableYellowBox=true
+YellowBox.ignoreWarnings(['Warning: ...'])
+console.disableYellowBox=true
 
 export default function App() {
   return (
