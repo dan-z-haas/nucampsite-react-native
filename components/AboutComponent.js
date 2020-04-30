403a71adb014e1daca80c78c8f90137b87aa89ca
Dan Haas <dan.z.haas@gmail.com>
2020-04-30 03:07:00 -0400

    activity indicator

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,6 +3,7 @@ import { Text, ScrollView, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 
 const mapStateToProps = state => {
@@ -19,32 +20,55 @@ class About extends Component {
 
     render() {
 
-        const renderPartner = ({item}) => {
+        const renderPartner = ({ item }) => {
             return (
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{source: {uri: baseUrl + item.image}}}
+                    leftAvatar={{ source: { uri: baseUrl + item.image } }}
                 />
             )
         }
 
         function Mission() {
-            return(
+            return (
                 <Card title="Our Mission">
                     <Text>We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.</Text>
                 </Card>
             )
         }
 
-        return(
+        if (this.props.partners.isLoading) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card title="Community Partners">
+                        <Loading />
+                    </Card>
+                </ScrollView>
+            )
+        }
+
+        if (this.props.partners.errMess) {
+            return (
+                <ScrollView>
+                    <Mission />
+                    <Card title="Community Partners">
+                        <Text>{this.props.parners.errMess}</Text>
+                    </Card>
+                </ScrollView>
+            )
+        }
+
+        return (
             <ScrollView>
                 <Mission />
                 <Card title="Community Partners">
                     <FlatList
-                            data={this.props.partners.partners}
-                            renderItem={renderPartner}
-                            keyExtractor={item => item.id.toString() } />
+                        data={this.props.partners.partners}
+                        renderItem={renderPartner}
+                        keyExtractor={item => item.id.toString()}
+                    />
                 </Card>
             </ScrollView>
         )
