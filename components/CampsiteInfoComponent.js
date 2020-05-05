2b243a29dbb03cb2558ffca972b3d45f0c61e403
Dan Haas <dan.z.haas@gmail.com>
2020-05-05 01:45:00 -0400

    favorites

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -3,7 +3,7 @@ import { Text, View, ScrollView, FlatList, Modal, Button, StyleSheet } from 'rea
 import { Card, Icon, Input, Rating } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
-import { postFavorite } from '../redux/ActionCreators';
+import { postFavorite, postComment } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
@@ -14,7 +14,8 @@ const mapStateToProps = state => {
 }
 
 const mapDispatchToProps = {
-    postFavorite: campsiteId => (postFavorite(campsiteId))
+    postFavorite: campsiteId => (postFavorite(campsiteId)),
+    postComment: (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text))
 }
 
 function RenderCampsite(props) {
@@ -63,7 +64,7 @@ function RenderComments({ comments }) {
                 <Text style={{ fontSize: 14 }}>{item.text}</Text>
                 <Rating 
                     startingValue={item.rating}
-                    imageSize='10'
+                    imageSize={10}
                     style={{alignItems:"flex-start", paddingVertical:"5%"}}
                     readonly
                 />
@@ -98,6 +99,7 @@ class CampsiteInfo extends Component {
     static navigationOptions = {
         title: 'Campsite Information'
     };
+
     markFavorite(campsiteId) {
         this.props.postFavorite(campsiteId);
     }
@@ -107,7 +109,7 @@ class CampsiteInfo extends Component {
     }
 
     resetForm() {
-        this.setState=({
+        this.setState({
             showModal: false,
             rating:5,
             author:'',
@@ -116,7 +118,7 @@ class CampsiteInfo extends Component {
     }
 
     handleComment(campsiteId) {
-        console.log(JSON.stringify(this.state));
+        this.props.postComment(campsiteId, this.state.rating, this.state.author, this.state.text);
         this.toggleModal();
     }
 
@@ -124,6 +126,7 @@ class CampsiteInfo extends Component {
         const campsiteId = this.props.navigation.getParam('campsiteId');
         const campsite = this.props.campsites.campsites.filter(campsite => campsite.id === campsiteId)[0];
         const comments = this.props.comments.comments.filter(comment => comment.campsiteId === campsiteId);
+
         return (
             <ScrollView>
                 <RenderCampsite
@@ -142,48 +145,32 @@ class CampsiteInfo extends Component {
                         <Rating
                             showRating
                             startingValue={this.state.rating}
-                            imageSize='40'
-                            onFinishRating={(rating)=>this.setState({rating: rating})} 
-                            style={{paddingVertical: 10}}
+                            imageSize={40}
+                            onFinishRating={(rating) => this.setState({rating: rating})} 
+                            style={{paddingVertical: 10}}             
                         ></Rating>
                         <Input
                             placeholder="Author"
-                            leftIcon={
-                                <Icon 
-                                    name={'user-o'}
-                                    type='font-awesome'
-                                    color='#5637DD'
-                                    raised
-                                    reverse
-                                    style={styles.cardItem}       
-                                />}
-                            leftIconContainerStyle={{paddingRight:10}}
-                            onChangeText={(rating)=>this.setState({rating: rating})} 
-                            value={this.state.rating}
+                            leftIcon={{name:'user-o', type:'font-awesome'}}                  
+                            leftIconContainerStyle={{paddingRight:10}}  
+                            onChangeText={(author)=>this.setState({author: author})} 
+                            value={this.state.author}
                         />
                         <Input
                             placeholder="Comment"
-                            leftIcon={
-                                <Icon 
-                                    name={'comment-o'}
-                                    type='font-awesome'
-                                    color='#5637DD'
-                                    raised
-                                    reverse
-                                    style={styles.cardItem}       
-                                />}
-                            leftIconContainerStyle={{paddingRight:10}}
-                            onChangeText={(rating)=>this.setState({rating: rating})} 
-                            value={this.state.rating}
+                            leftIcon={{name:'comment-o', type:'font-awesome'}}
+                            leftIconContainerStyle={{paddingRight:10}}  
+                            onChangeText={(text)=>this.setState({text: text})} 
+                            value={this.state.text}
                         />
-                        <View style={{ margin: 10 }} >
+                        <View style={{ margin: 10 }} >              
                             <Button
                                 onPress={() => {this.handleComment(campsiteId);this.resetForm()}}
                                 color='#5637DD'
                                 title="Submit"
                             />
                         </View>
-                        <View style={{ margin: 10 }} >
+                        <View style={{ margin: 10 }} >             
                             <Button
                                 onPress={() => {this.toggleModal(); this.resetForm()}}
                                 color='#808080'
@@ -198,6 +185,7 @@ class CampsiteInfo extends Component {
 }
 
 const styles = StyleSheet.create({
+    
     cardRow: {
         alignItems: 'center',
         justifyContent: 'center',
